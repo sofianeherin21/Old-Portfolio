fb8be792521a352bcb6910404fa7f653395ab30e
Sofia Neherin <sofia.neherin@gmail.com>
2020-07-25 22:41:42 +0600

    Complete

--- js/scripts.js
+++ js/scripts.js
@@ -6,28 +6,18 @@ $(document).ready(function(){
                 if($("#carouselBtn").children("span").hasClass('fa-pause'))
                 {
                     $("#myCarousel").carousel('pause');
-                    $("#carouselBtn").children("span").removeClass('fa-pause')
-                    $("#carouselBtn").children("span").addClass('fa-play')
+                    $("#carouselBtn").children("span").removeClass('fa-pause');
+                    $("#carouselBtn").children("span").addClass('fa-play');
                 }
 
                 else if($("#carouselBtn").children("span").hasClass('fa-play'))
                 {
                     $("#myCarousel").carousel('cycle');
-                    $("#carouselBtn").children("span").removeClass('fa-play')
-                    $("#carouselBtn").children("span").addClass('fa-pause')
+                    $("#carouselBtn").children("span").removeClass('fa-play');
+                    $("#carouselBtn").children("span").addClass('fa-pause');
                 }
                 
             })
 
-
-            $("#loginbtn").click(function()
-            {
-                $("#loginModal").modal('show');
-            })
-
-            $("#reservebtn").click(function()
-            {
-                $("#reserveModal").modal('show');
-            })
 
         })
